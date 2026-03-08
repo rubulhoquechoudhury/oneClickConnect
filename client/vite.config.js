3bf945918510a689aca4ff27ef179e6ad362be47
Rubul Hoque Choudhury <rubulhoquechoudhury123@gmail.com>
2026-03-08 18:31:52 +0530

    Revert "fixing client"
    This reverts commit 3409816b2d693273932dd1c317a3503ea19a03de.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const backendUrl = env.BACKEND_URL;
   const backendPort = env.BACKEND_PORT || '5000';
-  const proxyTarget = backendUrl || `http://localhost:${backendPort}`;
 
   return {
     plugins: [react()],
@@ -13,7 +11,7 @@ export default defineConfig(({ mode }) => {
       port: env.VITE_PORT || 5173,
       proxy: {
         '/api': {
-          target: proxyTarget,
+          target: `http://localhost:${backendPort}`,
           changeOrigin: true
         }
       }
