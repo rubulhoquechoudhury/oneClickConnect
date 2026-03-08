3409816b2d693273932dd1c317a3503ea19a03de
Rubul Hoque Choudhury <rubulhoquechoudhury123@gmail.com>
2026-03-08 18:27:02 +0530

    fixing client

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  const backendUrl = env.BACKEND_URL;
   const backendPort = env.BACKEND_PORT || '5000';
+  const proxyTarget = backendUrl || `http://localhost:${backendPort}`;
 
   return {
     plugins: [react()],
@@ -11,7 +13,7 @@ export default defineConfig(({ mode }) => {
       port: env.VITE_PORT || 5173,
       proxy: {
         '/api': {
-          target: `http://localhost:${backendPort}`,
+          target: proxyTarget,
           changeOrigin: true
         }
       }
